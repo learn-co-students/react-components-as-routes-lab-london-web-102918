5c4b9229db2d05885011860cbfee37d7a24d2aa8
rosen777 <rosen.toshev@flatironschool.com>
2019-01-26 18:41:51 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { actors } from '../data';
 
-const Actors = () => {
+class Actors extends React.Component {
+
+  state = {
+      actors: actors
+  }
+
+  render() {
   return (
-    <div>
-      {/*{code here}*/}
+    <div >
+    <h1>Actors Page</h1>
+
+        {actors.map((actor) => (
+        <div className='actor'>
+          <ul>
+          <li>Name: {actor.name}</li>
+          <li>Movies: {actor.movies.join(', ')}</li>
+          </ul>
+        </div>
+        ))}
     </div>
-  );
+    );
+  }
 };
 
 export default Actors;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route, NavLink
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
 import Directors from '../components/Directors';
 import Movies from '../components/Movies';
+import { actors } from '../data';
 
 
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+    <React.Fragment>
+    <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/actors" component={Actors} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/movies" component={Movies} />
+    </React.Fragment>
     </Router>
   );
 };
