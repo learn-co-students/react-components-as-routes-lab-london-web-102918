84f8d7e94a7c6813db089c4d763b052d163fed77
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-01-28 14:38:40 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Home from './Home';
+import Movies from './Movies';
 
-const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
 
+class NavBar extends Component {
+  render() {
+    return (
+<div className="navbar">
+    <li>
+      <NavLink
+      to="/"
+      exact
+      >Home</NavLink>
+      </li>
+      <li>
+      <NavLink
+      to="/movies"
+      exact
+      >Movies</NavLink>
+      </li>
+      <li>
+      <NavLink
+      to="/directors"
+      exact
+      >Directors</NavLink>
+      </li>
+      <li>
+      <NavLink
+      to="/actors"
+      exact
+      >Actors</NavLink>
+    </li>
+</div>
+
+  )
+}
+}
 export default NavBar;
